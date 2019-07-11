441a8a4b5554ff417a01acc8b60ee5bdc260820e
Ladus <roy_biesheuvel@hotmail.com>
2019-07-11 10:04:28 +0200

    0.0.2 - Added optional case insensitivity, and partial matching

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 "use strict";
-String.prototype.like = function (likeCondition) {
-    var thisString = this.toString();
+String.prototype.like = function (likeCondition, matchPartial, caseSensitive) {
+    if (matchPartial == undefined) {
+        matchPartial = false;
+    }
+    if (caseSensitive == undefined) {
+        caseSensitive = true;
+    }
     //Escape regex characters from likeCondition
     likeCondition = likeCondition.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-    var likeRegexString = "^" + likeCondition.replace(/\%/g, ".*") + "$";
+    var thisString = this.toString();
+    if (!caseSensitive) {
+        thisString = thisString.toUpperCase();
+        likeCondition = likeCondition.toUpperCase();
+    }
+    var likeRegexString = likeCondition.replace(/\%/g, ".*");
+    if (!matchPartial) {
+        likeRegexString = "^" + likeRegexString + "$";
+    }
     var likeRegexPattern = new RegExp(likeRegexString);
     if (likeRegexPattern.test(thisString)) {
         return true;
--- index.ts
+++ index.ts
@@ -1,14 +1,31 @@
 declare interface String {
-    like(likeCondition : string) : boolean;
+    like(likeCondition : string, matchPartial?: boolean, caseSensitive?: boolean) : boolean;
 }
 
-String.prototype.like = function ( likeCondition: string ): boolean {
-    let thisString: string = this.toString();
+String.prototype.like = function ( likeCondition: string, matchPartial?: boolean, caseSensitive?: boolean): boolean {
 
+    if(matchPartial == undefined) {
+        matchPartial = false;
+    }
+    if(caseSensitive == undefined) {
+        caseSensitive = true;
+    }
+    
     //Escape regex characters from likeCondition
     likeCondition = likeCondition.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 
-    let likeRegexString: string = "^"+likeCondition.replace(/\%/g,".*")+"$";
+    let thisString: string = this.toString();
+    if(!caseSensitive){
+         thisString = thisString.toUpperCase();
+         likeCondition = likeCondition.toUpperCase();
+    }
+
+    let likeRegexString: string = likeCondition.replace(/\%/g,".*");
+
+    if(!matchPartial) {
+        likeRegexString = "^"+likeRegexString+"$";
+    }
+    
     let likeRegexPattern: RegExp = new RegExp(likeRegexString);
     
     if(likeRegexPattern.test(thisString)) {
